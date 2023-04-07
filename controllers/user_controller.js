caf240b83e0110a94f3706cf6971a2007edbf91f
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-04-07 16:20:17 +0800

    Added a class for uploading images

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,46 @@
-const { getStorage, ref, uploadBytes, getDownloadURL } = require("firebase/storage");
-const processImageMiddleware = require('../configs/multer-config.js');
-exports.get_fe = (req,res,next) => {
+const { 
+	getStorage, 
+	ref, 
+	uploadBytes, 
+	getDownloadURL
+} = require("firebase/storage");
+const frontEndTech = require('../models/frontEndTechnologies.js'); 
+const processImageMiddleware = require('../configs/multer-config.js'); // multer config for form-data
+class FirebaseImageOperator {
+	constructor(firebaseRef,modelObj,reqObj,resObj) {
+		this.firebaseRef = firebaseRef;
+		this.model = modelObj;
+		this.req = reqObj;
+		this.res = resObj;
+	}
+	//upload => fileBuffer:bufferBytes, fileMetadata:metadataObj ;
+		// uploads bytes to firebase storage and outputs url
+		// which to be use on new document from the schema
+		// returns if successful otherwise error
+	upload(fileBuffer,fileMetadata) {
+		uploadBytes(this.firebaseRef,fileBuffer,fileMetadata)
+			.then(()=> {
+				getDownloadURL(this.firebaseRef).then(url => {
+					const newfrontEndTech = new this.model({
+						name:this.req.body.name,
+						imgLocation:url
+					});
+					newfrontEndTech
+						.save()
+						.then((document)=> {
+						console.log("Added new Front End Technologies: ",document.name);
+						return this.res.json({message:"File Uploaded Successfully"});
+					})
+						.catch((err)=> {
+							return this.res.status(400).json({error:err});
+						});
+				});
+			})
+			.catch(err => this.res.status(400).json({error:err}));
+	}
+}
 
+exports.get_fe = (req,res,next) => {
 };
 exports.post_fe = [
 	processImageMiddleware,
@@ -15,14 +54,18 @@ exports.post_fe = [
 		const metadata = {
 		  contentType: req.file.mimetype
 		};
-		uploadBytes(imageRef,req.file.buffer,metadata).then(() => { //asynchronously uploads images
-			console.log('Uploaded a blob or file!');
-			getDownloadURL(imageRef).then((url)=>{ //gets the img URL
-				console.log("url:",url);
+		const firebaseImageOperator = new FirebaseImageOperator(imageRef,frontEndTech,req,res);
+		frontEndTech.findOne({name:req.body.name})
+			.then((result)=> {
+				if (result === null) {
+					firebaseImageOperator.upload(req.file.buffer,metadata);
+				}
+				else {
+					return res.status(400).json({message:"The name already exist in the database"});
+				}
+			})
+			.catch((err)=> {
+				return res.status(400).json({error:err});
 			});
-		}).catch((err) => {
-			return res.setStatus(400).json({error:err});
-		});
-		return res.json({message:"File Uploaded Successfully"});
 	}
 ];
