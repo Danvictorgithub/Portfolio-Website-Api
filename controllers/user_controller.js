4dc24c2f80208ba7470103313ba23b85f40ab77a
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-05-25 18:39:21 +0800

    Added Authentication Controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,7 +4,65 @@ const {
 	uploadBytes, 
 	getDownloadURL
 } = require("firebase/storage");
+const passport = require("passport");
+const User = require("../models/user.js");
+const bcrypt = require("bcryptjs");
+const {body,validationResult} = require("express-validator");
 const frontEndTech = require('../models/frontEndTechnologies.js'); 
+const jwt = require("jsonwebtoken");
+exports.login_POST = (req,res) => {
+	passport.authenticate('local',{session:false},(err,user,info) => {
+
+		// session is set to false since API is used for authentication
+		
+		if (err || !user) {
+			return res.status(400).json({
+				message: "Invalid Password",
+				user: user
+			});
+		}	
+		req.login(user, {session: false}, (err) => {
+           if (err) {
+               res.send(err);
+           }
+			jwt.sign({user}, process.env.SECRET_KEY,(err,token)=>{
+				if (err) {
+					return res.status(400).json({
+						message: "Something is not right",
+					});
+				}
+				return res.json({user, token});
+			});
+		   });
+	})(req,res);
+};
+exports.signup_POST = [
+	body('username').trim().isLength({min:8,max:16}).withMessage("Username is either less than 8 or greater than 16").escape(),
+	body('password').trim().isLength({min:8,max:128}).withMessage("Password is either less than 8 or greater than 128").escape(),
+	(req,res)=> {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({errors:errors.array()});
+		}
+		User.findOne({username:req.body.username}).then((result)=> {
+			if (result != undefined) {
+				return res.status(400).json({message:"User Already Exist"});
+			}
+			bcrypt.hash(req.body.password,12,(err,token)=> {
+				if (err) {
+					return res.status(400).json({message:"Unexpected Error"});
+				}
+				const newUser = new User({
+					username: req.body.username,
+					password: token,
+				});
+				newUser.save();
+				res.status(201).json({message:"Successfully created a new User"});
+			});
+		})
+		return;
+	}
+];
 const processImageMiddleware = require('../configs/multer-config.js'); // multer config for form-data
 class FirebaseImageOperator {
 	constructor(firebaseRef,modelObj,reqObj,resObj) {
